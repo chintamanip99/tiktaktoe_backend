96d0e8a2836a95699af337036b147bb1cf7c303e
chintamanip99 <chintamanip99@gmail.com>
2023-12-05 08:08:33 +0530

    [tiktaktoe_backend-1] Adding pythonanywhere db

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,10 @@ module Tiktaktoe
     config.hosts << "15.206.26.154"
     config.hosts << "192.168.29.101"
     config.hosts << "192.168.29.13"
+    config.hosts << "192.168.29.2"
     config.hosts << "tiktaktoe-backend.notmaniac.com"
     config.hosts << "tiktaktoe-backend.chintamanipatil.in"
-    config.action_cable.allowed_request_origins = ['http://192.168.1.19:3002/','http://192.168.10.100:3001/','http://192.168.1.3:8081/','http://192.168.1.9:8081/','http://192.168.1.5:3002/','http://15.206.26.154:3002/',"http://tiktaktoe.notmaniac.com/","http://192.168.29.101:3002","http://tiktaktoe.chintamanipatil.in"]
+    config.action_cable.allowed_request_origins = ['http://192.168.29.2:3002/','http://192.168.10.100:3001/','http://192.168.1.3:8081/','http://192.168.1.9:8081/','http://192.168.1.5:3002/','http://15.206.26.154:3002/',"http://tiktaktoe.notmaniac.com/","http://192.168.29.101:3002","http://tiktaktoe.chintamanipatil.in"]
 
     # expose_headers = ['Access-Token', 'Uid','Client','Token-Type','Expiry', 'Authorization']
     #
